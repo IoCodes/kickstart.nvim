095d23e7042cc34a48647970a6c8a745ce762b2f
mario <mariohayek17@gmail.com>
2024-12-15 11:00:48 +0200

    feat(nvim): configure internal terminal

--- init.lua
+++ init.lua
@@ -234,6 +234,24 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
+-- [[ Internal NVIM terminal configuration ]]
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Remove terminal numbers and relative numbers',
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.keymap.set('n', '<leader>st', function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd 'J'
+  vim.api.nvim_win_set_height(0, 15)
+end)
+-- [[ Internal NVIM terminal configuration ]]
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
